b65fe1f6d97559d0b63a7028b9755884c58153e8
AntoineWDG <antoinewendlinger@gmail.com>
2018-02-24 15:30:01 +0100

    Fix polybar

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias reload!='cd ~/.dotfiles && git pull && ./script/bootstrap'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias rmzdns='sudo cp ~/repos/teenager-setup/resolv.conf /etc'
+alias l='exa -la --group-directories-first'
