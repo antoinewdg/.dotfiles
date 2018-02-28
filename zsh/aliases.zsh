7af4aab2fd25a46b00ac21d23d7cb6aefe443564
Toto <antoine.wendlinger@reminiz.com>
2018-02-28 10:48:25 +0100

    Small changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,5 @@ alias reload!='cd ~/.dotfiles && git pull && ./script/bootstrap'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias rmzdns='sudo cp ~/repos/teenager-setup/resolv.conf /etc'
+alias rmzvpn='cd ~/.reminiz_vpn && sudo openvpn config.ovpn'
 alias l='exa -la --group-directories-first'
