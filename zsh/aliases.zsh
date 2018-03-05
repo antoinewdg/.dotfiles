0008cbe013e177199370b75a42de489c65a0d793
Toto <antoine.wendlinger@reminiz.com>
2018-03-05 16:36:22 +0100

    Add snoop alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,6 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias rmzdns='sudo cp ~/repos/teenager-setup/resolv.conf /etc'
 alias rmzvpn='cd ~/.reminiz_vpn && sudo openvpn config.ovpn'
 alias l='exa -la --group-directories-first'
+
+alias snoop='sudo strace -s9999 -e write -p'
+
