003b7d55d06a44a3062654fec38716ea53bad5dd
Antoine Wendlinger <awendlinger@mytraffic.fr>
2021-06-24 09:40:46 +0200

    Emacs config

---
+++ mytraffic/aliases.zsh
@@ -0,0 +1,2 @@
+alias sc="cd $HOME/work/spark-jar/scheduler && poetry run scheduler"
+alias seldon_launch="poetry run streamlit run $HOME/work/mytraffic-data/_launch.py"
--- python/path.zsh
+++ python/path.zsh
@@ -1,2 +1,3 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$HOME/.poetry/bin:$PATH"
+
