f59bcdf0fc21c15227c5f0b7a917a1eda9a3368f
Antoine Wendlinger <awendlinger@mytraffic.fr>
2021-09-14 12:08:46 +0200

    direnv

--- nix/setup.zsh
+++ nix/setup.zsh
@@ -1,2 +1,6 @@
 if [ -e /home/toto/.nix-profile/etc/profile.d/nix.sh ]; then . /home/toto/.nix-profile/etc/profile.d/nix.sh; fi
+# Init Home manager
+. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+
 
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,6 @@ alias reload!='pushd ~/.dotfiles && git pull && ./script/bootstrap && popd && so
 
 alias cls='clear' # Good 'ol Clear Screen command
 
-alias rmzdns='sudo cp ~/repos/teenager-setup/resolv.conf /etc'
-alias rmzvpn='cd ~/.reminiz_vpn && sudo openvpn config.ovpn'
 alias l='exa -la --group-directories-first'
 
 alias snoop='sudo strace -s9999 -e write -p'
