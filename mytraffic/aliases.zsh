f182137167cfefc3de4018b6bde1f2edc537ff3e
Antoine Wendlinger <awendlinger@mytraffic.fr>
2022-03-09 13:41:54 +0100

    Fish shell

--- mytraffic/aliases.zsh
+++ mytraffic/aliases.zsh
@@ -1,2 +1,2 @@
-alias sc="cd $HOME/work/spark-jar/scheduler && poetry run scheduler"
+alias sc="cd $HOME/work/spark-jar/scheduler && scheduler"
 alias seldon_launch="poetry run streamlit run $HOME/work/mytraffic-data/_launch.py"
